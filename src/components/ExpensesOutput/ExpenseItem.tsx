65601e3290abffbfb33e0c5a6ca31e150f7ed1d6
clouden <clouden@syneforge.com>
2026-08-16 17:02:40 +0300

    add util function for date formatting

--- src/components/ExpensesOutput/ExpenseItem.tsx
+++ src/components/ExpensesOutput/ExpenseItem.tsx
@@ -1,6 +1,7 @@
 import { Platform, Pressable, StyleSheet, Text, View } from 'react-native';
 
 import { GLOBAL_STYLES } from '@/constants/styles';
+import { getFormattedDate } from '@/utils/date';
 
 type ExpenseItemProps = {
 	description: string;
@@ -16,10 +17,10 @@ const ExpenseItem = ({ description, amount, date }: ExpenseItemProps) => {
 					<Text style={[styles.textBase, styles.description]}>
 						{description}
 					</Text>
-					<Text style={styles.textBase}>{date.toLocaleDateString()}</Text>
+					<Text style={styles.textBase}>{getFormattedDate(date)}</Text>
 				</View>
 				<View style={styles.amountContainer}>
-					<Text style={styles.amount}>{amount}</Text>
+					<Text style={styles.amount}>{amount.toFixed(2)}</Text>
 				</View>
 			</View>
 		</Pressable>
@@ -67,6 +68,7 @@ const styles = StyleSheet.create({
 		justifyContent: 'center',
 		alignItems: 'center',
 		borderRadius: 4,
+		minWidth: 80,
 	},
 	amount: {
 		color: GLOBAL_STYLES.colors.primary500,
