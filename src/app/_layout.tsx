6e55929afcdff009470b31cf38ce1dc74d746da3
clouden <clouden@syneforge.com>
2026-08-16 17:52:48 +0300

    add styles for manage expense screen

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -1,3 +1,4 @@
+import { GLOBAL_STYLES } from '@/constants/styles';
 import { Stack } from 'expo-router';
 import { StatusBar } from 'expo-status-bar';
 import {
@@ -13,11 +14,18 @@ export default function RootLayout() {
 				<Stack.Screen name="(tabs)" options={{ headerShown: false }} />
 				<Stack.Screen
 					name="expenses-overview"
-					options={{ headerShown: false }}
+					options={{ title: 'Expenses Overview' }}
 				/>
 				<Stack.Screen
 					name="manage-expense"
-					options={{ title: 'Manage Expense' }}
+					options={{
+						title: 'Manage Expense',
+						presentation: 'modal',
+						headerStyle: {
+							backgroundColor: GLOBAL_STYLES.colors.primary500,
+						},
+						headerTintColor: '#fff',
+					}}
 				/>
 			</Stack>
 		</SafeAreaProvider>
--- src/app/manage-expense/index.tsx
+++ src/app/manage-expense/index.tsx
@@ -1,15 +1,9 @@
-import { Stack } from 'expo-router';
 import { StyleSheet, Text } from 'react-native';
 import { SafeAreaView } from 'react-native-safe-area-context';
 
 const ManageExpensePage = () => {
 	return (
 		<SafeAreaView edges={['bottom', 'left', 'right']} style={styles.container}>
-			<Stack.Screen
-				options={{
-					headerBackTitle: 'Back',
-				}}
-			/>
 			<Text>Manage Expense Page</Text>
 		</SafeAreaView>
 	);
