eb711e1fc7b54d4a6245736acc2a8a7c8be03e3d
clouden <clouden@syneforge.com>
2026-08-16 18:09:21 +0300

    add delete button on manage expense screen

--- src/app/manage-expense/index.tsx
+++ src/app/manage-expense/index.tsx
@@ -1,10 +1,17 @@
 import { Stack, useLocalSearchParams } from 'expo-router';
-import { StyleSheet, Text } from 'react-native';
+import { StyleSheet, View } from 'react-native';
 import { SafeAreaView } from 'react-native-safe-area-context';
+
+import IconButton from '@/components/UI/IconButton';
+import { GLOBAL_STYLES } from '@/constants/styles';
 
 const ManageExpensePage = () => {
 	const { editedExpenseId } = useLocalSearchParams();
 	const isEditing = !!editedExpenseId;
+
+	const handleDeleteExpense = () => {
+		console.log('Delete expense');
+	};
 
 	return (
 		<SafeAreaView edges={['bottom', 'left', 'right']} style={styles.container}>
@@ -13,7 +20,16 @@ const ManageExpensePage = () => {
 					title: isEditing ? 'Edit Expense' : 'Add Expense',
 				}}
 			/>
-			<Text>Manage Expense Page</Text>
+			{isEditing && (
+				<View style={styles.deleteContainer}>
+					<IconButton
+						icon="trash"
+						size={36}
+						color={GLOBAL_STYLES.colors.error500}
+						onPress={handleDeleteExpense}
+					/>
+				</View>
+			)}
 		</SafeAreaView>
 	);
 };
@@ -23,5 +39,14 @@ export default ManageExpensePage;
 const styles = StyleSheet.create({
 	container: {
 		flex: 1,
+		padding: 24,
+		backgroundColor: GLOBAL_STYLES.colors.primary800,
+	},
+	deleteContainer: {
+		marginTop: 16,
+		paddingTop: 8,
+		borderTopWidth: 2,
+		borderTopColor: GLOBAL_STYLES.colors.primary200,
+		alignItems: 'center',
 	},
 });
