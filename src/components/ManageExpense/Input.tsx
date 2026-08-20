7fd8a1e943060957c25e18165177722665aa09ba
clouden <clouden@syneforge.com>
2026-08-20 21:58:17 +0300

    add input styles

--- src/components/ManageExpense/Input.tsx
+++ src/components/ManageExpense/Input.tsx
@@ -1,9 +1,12 @@
+import { GLOBAL_STYLES } from '@/constants/styles';
 import {
 	StyleSheet,
 	Text,
 	TextInput,
 	View,
+	type StyleProp,
 	type TextInputProps,
+	type TextStyle,
 } from 'react-native';
 
 type InputProps = {
@@ -12,14 +15,41 @@ type InputProps = {
 };
 
 const Input = ({ label, textInputConfig }: InputProps) => {
+	const inputStyles: StyleProp<TextStyle>[] = [styles.input];
+
+	if (textInputConfig && textInputConfig.multiline) {
+		inputStyles.push(styles.inputMultiline);
+	}
+
 	return (
-		<View>
-			<Text>{label}</Text>
-			<TextInput {...textInputConfig} />
+		<View style={styles.inputContainer}>
+			<Text style={styles.label}>{label}</Text>
+			<TextInput {...textInputConfig} style={inputStyles} />
 		</View>
 	);
 };
 
 export default Input;
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+	inputContainer: {
+		marginHorizontal: 4,
+		marginVertical: 8,
+	},
+	label: {
+		fontSize: 12,
+		color: GLOBAL_STYLES.colors.primary100,
+		marginBottom: 4,
+	},
+	input: {
+		backgroundColor: GLOBAL_STYLES.colors.primary100,
+		color: GLOBAL_STYLES.colors.primary700,
+		padding: 6,
+		borderRadius: 6,
+		fontSize: 18,
+	},
+	inputMultiline: {
+		minHeight: 100,
+		textAlignVertical: 'top',
+	},
+});
